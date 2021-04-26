363962aa7fef33c4411e0aad47ed616685b2e3fa
Jennifer <maston@un.org>
2021-04-26 11:49:15 -0400

    added table templates

--- tor/__init__.py
+++ tor/__init__.py
@@ -9,16 +9,19 @@ def welcome():
 #Route for GA Conventions & Declarations by language
 @app.route('/<string:lang>/ga/conventions')
 def ga_conventions(lang):
+    table_txt = get_table_info(lang, "GA", "Conventions")
     return 'Route for GA Conventions & Declarations by language'
 
 #Route for GA Special Sessions by language
 @app.route('/<string:lang>/ga/special')
 def ga_special(lang):
+    table_txt = get_table_info(lang, "GA", "Special Sessions")
     return 'Route for GA Special Sessions by language'
 
 #Route for GA Emergency Special Sessions by language
 @app.route('/<string:lang>/ga/emergency')
 def ga_emergency(lang):
+    table_txt = get_table_info(lang, "GA", "Emergency Sessions")
     return 'Route for GA Emergency Special Sessions'
 
 #Route for GA Regular Session by language and session
@@ -30,11 +33,13 @@ def ga_regular(lang, session):
 #Route for SC Veteos by language
 @app.route('/<string:lang>/sc/veteos')
 def sc_vetoes(lang, session):
+    table_txt = get_table_info(lang, "SC", "Vetoes")
     return 'Route for SC Veteos by language'
 
 #Route for SC Meetings & Outcomes by language and year
 @app.route('/<string:lang>/sc/meetings/<int:year>')
 def sc_meetings(lang, year):
+    table_txt = get_table_info(lang, "SC", "Meetings and Outcomes")
     return 'Route for SC Meetings & Outcomes by language and year'
 
 @app.errorhandler(404)
